6b8146d57e309f2a61245a6d92207eb335d0d070
ManuelCaroDev <manuelcaro.dev@gmail.com>
2025-12-17 12:44:08 +0100

    Ejercicio4

---
+++ src/PB_Ejercicio4/Main.java
@@ -0,0 +1,17 @@
+package PB_Ejercicio4;
+
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        ProcessBuilder pb = new ProcessBuilder();
+        Map<String, String> entorno = pb.environment();
+
+        System.out.println("Varianbles de entorno del sistema");
+
+        for (String clave: entorno.keySet()){
+            System.out.println(clave+ " = "+entorno.get(clave));
+        }
+    }
+
+}
